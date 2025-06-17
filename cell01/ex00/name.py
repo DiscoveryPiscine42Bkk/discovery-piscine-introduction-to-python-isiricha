e3d72dbfde404f52770124b1c1a59c44c6dacdd5
isiricha <ittikorn2309@gmail.com>
2025-06-17 06:43:22 +0000

    01

---
+++ cell01/ex00/name.py
@@ -0,0 +1,3 @@
+first_name = "Ittikorn"
+last_name = "Siricharoen"
+print(first_name, last_name)
---
+++ cell01/ex01/name.py
@@ -0,0 +1,4 @@
+first_name = "Ittikorn"
+last_name = "Siricharoen"
+whole_name = first_name + " " + last_name
+print(whole_name)
